193ffebdee4465298ec179803f1a1358339d90bf
Juan.David.Mosquera.Palacios <juandavidmospa12@gmail.com>
2025-11-04 20:24:27 -0500

    Subida inicial de archivos del proyecto Guia1_practica

--- programa_01.cpp
+++ programa_01.cpp
@@ -1,6 +1,12 @@
+#ifdef __cpp_hex_float
+// __cpp_hex_float is defined as a feature-test macro; store its value
+constexpr long cpp_hex_float_value = __cpp_hex_float;
+#else
+// Not defined; set to 0
+constexpr long cpp_hex_float_value = 0;
+#endif
+
 int main() {
-    // Escriba un programa que imprima "Hola mundo" en la pantalla.
-    ("Hola mundo\n");
-    return 0;
-    
+	(void)cpp_hex_float_value;
+	return 0;
 }
